e07f97b97a416a32dcdffdf4175f99f0cf0475c7
x-junzhu <x_junzhu@163.com>
2020-04-03 21:07:17 +0800

    前缀和

---
+++ BaseAlgorithm/PrefixSum795-0403Reback.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 100010;
+
+int n, m;
+int a[N], s[N];
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for(int i = 1; i <= n; i++) scanf("%d", &a[i]);
+
+    //预处理前缀和
+    for(int i = 1; i <= n; i++) s[i] = s[i - 1] + a[i];
+
+    while(m--)
+    {
+        int l, r;
+        scanf("%d%d", &l, &r);
+        cout << s[r] - s[l - 1] << endl;
+    }
+    return 0;
+}
