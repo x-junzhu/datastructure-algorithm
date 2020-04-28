d3296df0410e8d2a2b347cfbeb374f9e2454c3f0
x-junzhu <x_junzhu@163.com>
2020-04-28 10:31:59 +0800

    欧拉函数

---
+++ Math/Euler873-0426.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    while(n--)
+    {
+        int a;
+        cin >> a;
+
+        int res = a;
+        for(int i = 2; i < a / i; i++)
+        {
+            if(a % i == 0)
+            {
+                res = res / i * (i - 1);
+                while(a % i == 0) a /= i;
+            }
+        }
+
+        if(a > 1) res = res / a * (a - 1);
+        cout << res << endl;
+    }
+    return 0;
+}
