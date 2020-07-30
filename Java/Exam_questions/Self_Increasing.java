19fa96fa08d3aa970ebfb29378931f79bc161e1b
x-junzhu <x_junzhu@163.com>
2020-07-30 14:33:59 +0800

    关于i++

---
+++ Java/Exam_questions/Self_Increasing.java
@@ -0,0 +1,18 @@
+package edu.fzu.search;
+
+/**
+ * @author johnrambo
+ * @create 2020-07-30 14:31
+ */
+public class Self_Increasing {
+
+    public static void main(String[] args){
+        int i = 1;
+        i = i++;
+        int j = i++;
+        int k = i +++i * i++;
+        System.out.println("i=" + i);
+        System.out.println("j=" + j);
+        System.out.println("k=" + k);
+    }
+}
